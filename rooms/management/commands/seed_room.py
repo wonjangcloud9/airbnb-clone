2d3e1d6b5245a7653d8c3a846209ba8c8cdc2b39
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-01-11 22:34:07 +0900

    add photo seed

--- rooms/management/commands/seed_room.py
+++ rooms/management/commands/seed_room.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 from django_seed import Seed
-
+from django.contrib.admin.utils import flatten
 from rooms import models as room_models
 from users import models as user_models
 import random
@@ -38,5 +38,14 @@ class Command(BaseCommand):
                 "baths": lambda x: random.randint(1, 5),
             },
         )
-        seeder.execute()
+        created_photos = seeder.execute()
+        created_clean = flatten(list(created_photos.values()))
+        for pk in created_clean:
+            room = room_models.Room.objects.get(pk=pk)
+            for i in range(3, random.randint(10, 17)):
+                room_models.Photo.objects.create(
+                    caption=seeder.faker.sentence(),
+                    room=room,
+                    file=f"/room_photos/{random.randint(1, 31)}.webp",
+                )
         self.stdout.write(self.style.SUCCESS(f"{number} rooms created"))
