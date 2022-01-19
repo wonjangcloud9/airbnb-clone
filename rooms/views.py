3102d8f4113d3a1c14e99f58c937bbff36ce99b5
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-01-19 21:14:40 +0900

    cbv DetailView

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,5 @@
 from django.utils import timezone
-from django.http import Http404
-from django.views.generic import ListView
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from . import models
 
 
@@ -22,9 +20,8 @@ class HomeView(ListView):
         return context
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        raise Http404()
+class RoomDetail(DetailView):
+
+    """RoomDetail Definition"""
+
+    model = models.Room
