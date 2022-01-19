6b35c6af394915a3261e652d8f8e772642736af8
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-01-19 21:50:44 +0900

    start search

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,6 @@
 from django.utils import timezone
 from django.views.generic import ListView, DetailView
+from django.shortcuts import render
 from . import models
 
 
@@ -25,3 +26,9 @@ class RoomDetail(DetailView):
     """RoomDetail Definition"""
 
     model = models.Room
+
+
+def search(request):
+    city = request.GET.get("city")
+    city = str.capitalize(city)
+    return render(request, "rooms/search.html", {"city": city})
