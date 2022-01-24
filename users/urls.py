2d0968918b0bf384d6b803d50a27233e80009ff3
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-01-25 07:34:19 +0900

    before login go setting

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+app_name = "users"
+
+urlpatterns = [
+    path(
+        "login",
+        views.LoginView.as_view(),
+        name="login",
+    )
+]
