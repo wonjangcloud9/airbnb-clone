9c32b6f1919a8d5473ca37ed39ec1eea1ca66f92
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-02-04 00:06:37 +0900

    tailwindcss not running

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "i$@5cxu-=9g_%37mj9g(o=+z*%ji)-xz4=8pz_%tv05&_&+0te"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
