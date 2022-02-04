73a5c6465f9d6629c21a0666fe2da8e01fa177f3
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-02-05 00:25:49 +0900

    tailwindcss header

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,9 @@ const gulp = require("gulp");
 
 const css = () => {
   const postCSS = require("gulp-postcss");
-  const sass = require("gulp-sass")(require("sass"));
+  const sass = require("gulp-sass");
   const minify = require("gulp-csso");
   sass.compiler = require("node-sass");
-
   return gulp
     .src("assets/scss/styles.scss")
     .pipe(sass().on("error", sass.logError))
