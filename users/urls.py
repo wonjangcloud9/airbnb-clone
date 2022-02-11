c1d1674794ea7068aecfaa9c90943c77a3906cbd
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-02-11 23:45:03 +0900

    update profile

--- users/urls.py
+++ users/urls.py
@@ -6,44 +6,49 @@ app_name = "users"
 
 urlpatterns = [
     path(
-        "login",
+        "login/",
         views.LoginView.as_view(),
         name="login",
     ),
     path(
-        "login/github",
+        "login/github/",
         views.github_login,
         name="github-login",
     ),
     path(
-        "login/github/callback",
+        "login/github/callback/",
         views.github_callback,
         name="github-callback",
     ),
     path(
-        "login/kakao",
+        "login/kakao/",
         views.kakao_login,
         name="kakao-login",
     ),
     path(
-        "login/kakao/callback",
+        "login/kakao/callback/",
         views.kakao_callback,
         name="kakao-callback",
     ),
     path(
-        "logout",
+        "logout/",
         views.log_out,
         name="logout",
     ),
     path(
-        "signup",
+        "signup/",
         views.SignUpView.as_view(),
         name="signup",
     ),
     path(
-        "verify/<str:key>",
+        "verify/<str:key>/",
         views.complete_verification,
         name="complete-verification",
+    ),
+    path(
+        "update-profile/",
+        views.UpdateProfileView.as_view(),
+        name="update",
     ),
     path(
         "<int:pk>/",
