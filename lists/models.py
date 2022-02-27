501a5498023c4a70c0934ca728400c6c4325c6e4
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-02-27 23:19:55 +0900

    on_favs tag

--- lists/models.py
+++ lists/models.py
@@ -8,7 +8,7 @@ class List(core_models.TimeStampedModel):
     """List Model Definition"""
 
     name = models.CharField(max_length=80)
-    user = models.ForeignKey("users.User", related_name="lists", on_delete=CASCADE)
+    user = models.OneToOneField("users.User", related_name="lists", on_delete=CASCADE)
     rooms = models.ManyToManyField("rooms.Room", related_name="lists", blank=True)
 
     def __str__(self):
